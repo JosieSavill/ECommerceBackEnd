93517d58f75a67560defa3322babff23e71169de
JosieSavill <fiordasavilljosie@gmail.com>
2023-04-19 02:18:20 -0700

    added  hasMany and  belongsToMany methods to models on index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,28 @@
-// import models
+// import models - josie
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category  - josie
+const Category = require('./Category');
+const Product = require('./Product');
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Categories have many Products - josie
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Products belongToMany Tags (through ProductTag) - josie 
+Product.belongsToMany(Tag, {through: ProductTag});
+
+// Tags belongToMany Products (through ProductTag) - josie
+Tag.belongsToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,
